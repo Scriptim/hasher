7867c9b0fa3a86b407eeb723ad340cbfcd246c02
Scriptim <Scriptim@gmx.de>
2017-08-01 22:24:28 +0200

    basic hashing script

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const submit = document.getElementById('submit')
+const main = document.getElementsByTagName('main')[0];
 
 const handler = function() {
-  var string = document.getElementById('string').value
-  //var salt = document.getElementById('salt').value
-  //var key = document.getElementById('key').value
+  var string = encodeURIComponent(document.getElementById('string').value)
+  //var salt = encodeURIComponent(document.getElementById('salt').value)
+  //var key = encodeURIComponent(document.getElementById('key').value)
 
-  // TODO: algorithm
+  var request = new XMLHttpRequest()
+  request.onload = function() {
+    if (this.status !== 200) {
+      alert('An error occured')
+      return
+    }
+    main.innerHTML = this.responseText
+  };
+  //request.open('GET', 'hash.php?str=' + string + '&salt=' + salt + '&key=' + key)
+  request.open('GET', 'hash.php?str=' + string)
+  request.send()
 }
 
 if (submit.addEventListener) {
-  submit.addEventListener("click", handler, false)
+  submit.addEventListener('click', handler, false)
 } else {
   submit.attachEvent('onclick', handler)
 }
